a47e991fde6a749647f200a730af18f9b2851313
HudsonDanielle-FS <110405791+HudsonDanielle-FS@users.noreply.github.com>
2023-08-02 10:05:02 -0500

    Routes complete

--- index.js
+++ index.js
@@ -12,19 +12,26 @@ app.post("/", (req, res) => {
   res.send("Home Page! POST...");
 });
 
-// GET /products/683-nike-large-white-shoe HTTP/1.1
+// GET /products/8719-small-red  HTTP/1.1
 app.get("/products/:productId-:productName", (req, res) => {
-    res.send(
-      `Product Page! product name: ` +
-        req.params.productName +
-        ` Product id: ` +
-        req.params.productId
-    );
-  });
+  res.send(
+    "GET Products: " + req.params.productId + ", " + req.params.productName
+  );
+});
 
-// GET /products/nike-large-white-shoe HTTP/1.1
-app.get("/products/:productName", (req, res) => {
-  res.send(`Product Page! product name: ` + req.params.productName);
+// GET /products/8719 HTTP/1.1
+app.get("/products/:productId", (req, res) => {
+  res.send(`GET Products: ` + req.params.productId);
+});
+
+// GET /products/all HTTP/1.1
+app.get("/products/:productPage-:productSort-:productOrder", (req, res) => {
+  res.send(
+    "GET Products: " +
+      req.headers.productPage +
+      req.headers.productSort +
+      req.headers.Order
+  );
 });
 
 app.listen(3000);

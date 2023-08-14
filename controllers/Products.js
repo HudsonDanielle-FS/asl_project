3740cb80dbed5dd1d858e9583cefb6a9e1ba46b0
HudsonDanielle-FS <110405791+HudsonDanielle-FS@users.noreply.github.com>
2023-08-13 19:49:54 -0500

    delete function working

--- controllers/Products.js
+++ controllers/Products.js
@@ -6,7 +6,6 @@ const index = (req, res) => {
 }
 
 const form = (req, res) => {
-    //res.send('Product.form')
     if (req.params.id) {
         const product = Products.find(req.params.id)
         res.render('views/products/edit', { product })
@@ -17,25 +16,21 @@ const form = (req, res) => {
 
 const show = (req, res) => {
     const product = Products.find(req.params.id)
-    //res.json(product)
     res.render('views/products/show', { product })
 }
 
 const create = (req, res) => {
     const product = Products.create(req.body)
-    //res.json(product)
     res.redirect('/products/' + product.id)
 }
 
 const update = (req, res) => {
     const product = Products.update(req.params.id, req.body)
-    //res.json(product)
     res.redirect('/products/' + req.params.id)
 }
 
 const remove = (req, res) => {
     const products = Products.remove(req.params.id)
-    //res.json(products)
     res.redirect('/products')
 }
 

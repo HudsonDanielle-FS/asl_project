ec8773ae2e8219fa61a658a33d5fb2f8f1aecaf3
HudsonDanielle-FS <110405791+HudsonDanielle-FS@users.noreply.github.com>
2023-08-24 17:22:17 -0500

    corrected mistakes in product.js

--- models/product.js
+++ models/product.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Product extends Model {
     /**
@@ -11,18 +9,21 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.Product.hasMany(models.Variant)
+      models.Product.hasMany(models.Variant);
     }
   }
-  Product.init({
-    title: DataTypes.STRING,
-    slug: DataTypes.STRING,
-    description: DataTypes.TEXT('long'),
-    price: DataTypes.INTEGER,
-    is_published: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Product',
-  });
+  Product.init(
+    {
+      title: DataTypes.STRING,
+      slug: DataTypes.STRING,
+      description: DataTypes.TEXT("long"),
+      price: DataTypes.INTEGER,
+      is_published: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Product",
+    }
+  );
   return Product;
 };
